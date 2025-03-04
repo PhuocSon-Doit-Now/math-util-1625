1847be21811f94b65dcea4c8328c77c3550fd8d6
PhuocSon-Doit-Now <nps05062003@gmail.com>
2025-03-04 19:14:56 +0700

    refactor the main code and green 4ever always

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ### You will find in this repo the following stuff:
 * The Math-Util library that offers multiple math functions (a Java-based source code)
 * Unit Testing source code with JUnit framework
+* GitHub Actions to demonstrate the Continuous Integration (CI) technique
+* Regression Test 
 * ...
 ### Screenshots
 ![DDT & TDD with JUnit](https://github.com/PhuocSon-Doit-Now/math-util-1625/blob/main/images/DDT%20with%20JUnit.png)
--- src/com/phuocson/mathutil/core/MathUtil.java
+++ src/com/phuocson/mathutil/core/MathUtil.java
@@ -22,16 +22,41 @@ public class MathUtil {
     //20 giai thừa 18 con số, vừa kịp đủ cho kiểu long của Java
     //21 giai thừa tràn kiểu long
     //bài này quy ước tính n! trong khoảng từ 0...20
-    public static long getFactrial(int n){
-        if (n < 0  || n > 20)
-            throw new IllegalArgumentException("Invalid argument. N must be between 0...20");
+//    public static long getFactrial(int n){
+//        if (n < 0  || n > 20)
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0...20");
+//        if (n == 0 | n == 1)
+//            return 1; //kết thúc cuộc chơi sớm nếu nhận những đầu vào đặc biệt 
+//        
+//        long product = 1;//tích nhân dồn, Thuật toán heo đất, ốc bu dồn thịt
+//        
+//        for (int i = 2; i <= n; i++)
+//            product *= i; //product = product * i;
+//        return product;
+//    }
+    
+    //tính dùm tui 6!
+    //n! = 1.2.3.4.5.6. ... .n
+    //6! = 6 X 5!
+    //5! = 5 X 4!
+    //4! = 4 X 3!
+    //3! = 3 X 2!
+    //2! = 2 X 1! //điểm dừng
+    //1! = 1 X 1!
+    
+    //quy ước 1! = 0! = 1
+    
+    //chốt hạ: n! = n * (n - 1)!
+     public static long getFactrial(int n){
+        if (n < 0 || n > 20)
+            throw new IllegalArgumentException("n must be between 0..20. plz");
         if (n == 0 | n == 1)
             return 1; //kết thúc cuộc chơi sớm nếu nhận những đầu vào đặc biệt 
         
-        long product = 1;//tích nhân dồn, Thuật toán heo đất, ốc bu dồn thịt
-        
-        for (int i = 2; i <= n; i++)
-            product *= i; //product = product * i;
-        return product;
+        return n * getFactrial(n - 1); //công thức đệ quy
     }
+    //Học để quy trong vòng 30s 
+    //Hiện tượng gọi lại chính mình với quy mô khác
+    //Ví dụ: con búp bê Nga, giống nhau và lồng trong nhau
+    //búp bê to, nhỏ hơn, nhỏ hơn nữa, nhỏ hơn nữa... đến điểm dừng
 }
